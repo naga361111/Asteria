0e65a2865d0bf0a56f17ea65f7279db59836e975
naga361111 <naga361111@gmail.com>
2026-08-31 15:44:21 +0900

    feat(npc): implement arrival deceleration and slowdown logic in NpcAIController
    - Add SlowdownRadius and ArrivalSpeed properties to ANpcAIController
    - Adjust CharacterMovement MaxWalkSpeed in Tick based on distance to path destination
    - Update NPC blueprints and behavior tree assets

--- Source/Asteria/NPC/NpcAIController.cpp
+++ Source/Asteria/NPC/NpcAIController.cpp
@@ -3,32 +3,75 @@
 
 #include "NPC/NpcAIController.h"
 
+#include "AsteriaNpc.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "Kismet/GameplayStatics.h"
+#include "Navigation/PathFollowingComponent.h"
+
+ANpcAIController::ANpcAIController()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ANpcAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	UCharacterMovementComponent* CMC = GetPawn<ACharacter>()->GetCharacterMovement();
+	if (CMC != nullptr)
+	{
+		if (GetPathFollowingComponent()->GetStatus() != EPathFollowingStatus::Moving)
+		{
+			CMC->MaxWalkSpeed = BaseMaxSpeed;
+			return;
+		}
+
+		FVector GoalActor = GetPathFollowingComponent()->GetPathDestination();
+		if (FAISystem::IsValidLocation(GoalActor))
+		{
+			float Dist = FVector::Dist2D(GetPawn()->GetActorLocation(), GoalActor);
+
+			if (Dist >= SlowdownRadius) { CMC->MaxWalkSpeed = BaseMaxSpeed; }
+			else
+			{
+				CMC->MaxWalkSpeed = FMath::Lerp(ArrivalSpeed, BaseMaxSpeed, Dist / SlowdownRadius);
+			}
+		}
+	}
+}
 
 void ANpcAIController::OnPossess(APawn* InPawn)
 {
 	Super::OnPossess(InPawn);
 	
+	UCharacterMovementComponent* CMC = GetPawn<ACharacter>()->GetCharacterMovement();
+	if (CMC != nullptr)
+	{
+		if (BaseMaxSpeed == 0)
+		{
+			BaseMaxSpeed = CMC->GetMaxSpeed();
+		}
+	}
+
 	TArray<AActor*> PostQuestBoardActor;
 	UGameplayStatics::GetAllActorsWithTag(GetWorld(), FName("PostQuestBoard"), PostQuestBoardActor);
-	
+
 	TArray<AActor*> NpcHomeActor;
 	UGameplayStatics::GetAllActorsWithTag(GetWorld(), FName("NpcHome"), NpcHomeActor);
-	
+
 	if (BehaviorTree != nullptr)
 	{
 		RunBehaviorTree(BehaviorTree);
-		
+
 		if (PostQuestBoardActor.Num() != 0)
 		{
 			GetBlackboardComponent()->SetValueAsObject(FName("PostQuestBoard"), PostQuestBoardActor[0]);
 		}
-		
+
 		if (NpcHomeActor.Num() != 0)
 		{
 			GetBlackboardComponent()->SetValueAsObject(FName("NpcHome"), NpcHomeActor[0]);
-
 		}
 	}
 }
--- Source/Asteria/NPC/NpcAIController.h
+++ Source/Asteria/NPC/NpcAIController.h
@@ -14,9 +14,21 @@ class ASTERIA_API ANpcAIController : public AAIController
 {
 	GENERATED_BODY()
 	
+	ANpcAIController();
+	
 public:
+	virtual void Tick(float DeltaSeconds) override;
+	
 	virtual void OnPossess(APawn* InPawn) override;
 	
 	UPROPERTY(EditDefaultsOnly, Category="BehaviorTree")
 	TObjectPtr<UBehaviorTree> BehaviorTree;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Movement")
+	float SlowdownRadius = 200.0f;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Movement")
+	float ArrivalSpeed = 50.0f;
+	
+	float BaseMaxSpeed = 0.0f;
 };
