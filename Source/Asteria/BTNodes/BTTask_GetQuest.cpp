0a5a6bb713f66a3c854e638c81c502601f65a8f2
naga361111 <naga361111@gmail.com>
2026-09-01 15:30:54 +0900

    feat(npc): implement BTTask_GetQuest execution and track accepted quests

--- Source/Asteria/BTNodes/BTTask_GetQuest.cpp
+++ Source/Asteria/BTNodes/BTTask_GetQuest.cpp
@@ -3,3 +3,15 @@
 
 #include "BTNodes/BTTask_GetQuest.h"
 
+#include "GameState/AsteriaGameState.h"
+
+EBTNodeResult::Type UBTTask_GetQuest::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAsteriaGameState* AGS = GetWorld()->GetGameState<AAsteriaGameState>();
+
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Cyan, FString::Printf(
+		                                 TEXT("[%d]Accepted quest id: %d"), GetWorld()->GetNetMode(),
+		                                 AGS->GetQuest()));
+
+	return EBTNodeResult::Succeeded;
+}
